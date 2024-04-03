5f7701a9f7343ca4f97c331ae6bfec8ac71298c6
Rockyroad14 <jaredreich12@gmail.com>
2024-04-02 20:38:52 -0400

    Added

--- mainOne.cpp
+++ mainOne.cpp
@@ -8,14 +8,21 @@
 #include <algorithm>
 #include <vector>
 #include <thread>
+#include <mutex>
 
 #define NUM_THREADS 4
+#define HASH_SIZE 100
 #define MAX 500000
 
 using namespace std;
 
-std::vector<thread> threads;
+struct HeadNode
+{
+    Node* next;
+    std::mutex m;
 
+    HeadNode() : next(nullptr) {}
+};
 
 struct Node
 {
@@ -26,10 +33,43 @@ struct Node
     Node(int id) : id(id), next(nullptr) {}
 };
 
-void servant(int id)
+std::vector<thread> threads;
+Node *head = nullptr;
+int* arr;
+atomic<int> index(0);
+std::mutex arrayLock;
+atomic<bool> flag(true);
+
+
+void servant()
 {
+    int localIndex;
+    int localValue;
+    // Loop until list is empty
+    while(flag)
+    {
+        // Seed the random number generator
+        srand(time(0));
+        // Make a random choice between 0-2 for the operation
+        int choice = rand() % 3;
+
+        arrayLock.lock();
+        localIndex = index;
+        index++;
+        localValue = arr[localIndex];
+        arrayLock.unlock();
+
+        
+
+
+
+
+
+
+    }
 
 }
+
 
 int* createArray(int* arr)
 {
@@ -43,8 +83,8 @@ int* createArray(int* arr)
 
     for (int i = 0; i < MAX; i++)
     {
-        int index = rand() % MAX + 1;
-        int index2 = rand() % MAX + 1;
+        int index = rand() % MAX;
+        int index2 = rand() % MAX;
         swap(arr[index], arr[index2]);
     }
 
@@ -54,8 +94,7 @@ int* createArray(int* arr)
 int main(void)
 {
     // create an array of 500,000 random positive non repeating integers
-    std::list<Node> list;
-    int* arr = (int*) malloc(MAX * sizeof(int));
+    arr = (int*) malloc(MAX * sizeof(int));
 
     arr = createArray(arr);
 
